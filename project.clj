33254a19edf3c1351d22aac7f82d4c8ed3db9022
Ernesto Alfonso <erjoalgo@gmail.com>
2016-05-29 11:50:58 -0700

    remove :resource-paths, add back clojure-mail, pending private maven repo

--- project.clj
+++ project.clj
@@ -5,14 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;;[smtp-clj "0.6.4"]
-                 ;;[io.forward/clojure-mail "1.0.4" ]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.logging "0.3.1"]
+                 [io.forward/clojure-mail "1.0.4" ]
                  ]
-  ;;TODO how to refer to forked version on github
-  :resource-paths ["/home/ealfonso/repos/clojure-mail/target/clojure-mail-1.0.4-standalone.jar"]
   :main ^:skip-aot smtp-contacts-cj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
