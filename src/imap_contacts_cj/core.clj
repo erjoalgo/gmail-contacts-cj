af55378c13247f404fb1728838e0c19f4de0b73f
Ernesto Alfonso <erjoalgo@gmail.com>
2016-06-03 23:32:56 -0700

    run batches in parallel

--- src/imap_contacts_cj/core.clj
+++ src/imap_contacts_cj/core.clj
@@ -52,6 +52,16 @@
   (flush)
   message)
 
+
+
+;https://github.com/clojure/clojure-contrib/blob/b8d2743d3a89e13fc9deb2844ca2167b34aaa9b6/src/main/clojure/clojure/contrib/seq.clj#L51
+(defn indexed
+  "Returns a lazy sequence of [index, item] pairs, where items come
+  from 's' and indexes count up from zero.
+  (indexed '(a b c d))  =>  ([0 a] [1 b] [2 c] [3 d])"
+  [s]
+  (map vector (iterate inc 0) s))
+
 (defn process-messages [db store & {:keys [folder max-messages newline quiet batch-size]
                                     :or {folder "INBOX"
                                          batch-size 100}}]
@@ -59,7 +69,7 @@
   (let [current-uid-validity (clojure-mail.core/get-folder-uid-validity
                               (clojure-mail.core/get-folder store folder))]
     (db/update-uid-validity-if-changed! db current-uid-validity))
-  
+
   (let [[smallest-uid largest-uid] (db/smallest-largest-known-uids db)
         messages (concat
                   ;;newer than our newest message, most recent first
@@ -74,24 +84,24 @@
                    messages)
 
 
-        message-pipeline (if (and nil quiet)
-                                messages
-                                (map-indexed
-                                 (fn [i message]
-                                   (echo-message quiet (count messages) i message)
-                                   message) messages))
+        total-message-count (count messages)
         ]
-    
+
     ;;make sure we're getting messages in ascending order
     (assert (or (-> messages nnext nil?)
                 (-> (juxt first second) (map clojure-mail.message/uid) >)))
-    
-    (doseq [part (partition batch-size
-                            (map (juxt clojure-mail.message/uid message-name-address-map-list)
-                                 message-pipeline))]
-      (let [name-address-map-lists (flatten (map second part))
-            uids (map first part)]
+
+    (doseq [[batch-number batch]
+            (indexed (pmap
+                      ;;map over each batch in parallel
+                      (partial map (juxt clojure-mail.message/uid message-name-address-map-list))
+                      (partition batch-size messages)))]
+      (printf "\r%d/%d" (* batch-size (inc batch-number)) total-message-count)
+      (flush)
+      (let [name-address-map-lists (flatten (map second batch))
+            uids (map first batch)]
         (when-not  quiet (printf "flushing... %s\n" (pr-str uids)))
+        ;(throw (Exception. "invalid imap server string"))
         (db/insert-name-address-to-db! db name-address-map-lists)
         (db/store-uids! db uids)))))
 
@@ -102,7 +112,7 @@
     (if (:errors args)
       (do (println (:summary args))
           (println (:errors args)))
-      
+
       (let [opts (:options args)
             max-results (:max-results opts)
             db-filename (:db opts)
@@ -113,7 +123,7 @@
             pass (if passwd-file (slurp passwd-file) (read-password :prompt "enter app specific pass: "))
 
             db (db/sqlite-db-connection-for-file db-filename)
-            
+
             mail-store (let [[protocol imap-host imap-port] (:imap-protocol-host-port opts)
                              protocol (case protocol
                                         "http" "imap"
@@ -121,7 +131,7 @@
                          ;;gstore (clojure-mail.gmail/store email pass) ;;gmail-specific
                          ;(clojure-mail.core/store "imaps" ["imap.gmail.com" 993] email pass)
                          (clojure-mail.core/store protocol [imap-host imap-port] email pass))]
-        
+
         ;;(db/ensure-tables-exist db/db :drop true)
         (db/ensure-tables-exist db)
         (process-messages db mail-store
@@ -132,5 +142,5 @@
 
 
 ;; Local Variables:
-;; compile-command: "lein run -- -m 100 --db ~/.imap-contacts.db -e erjoalgo@gmail.com -p ~/repos/imap-contacts-cj/pass -s https://imap.gmail.com:993"
+;; compile-command: "lein run -- -m 0 --db ~/.imap-contacts.db -e erjoalgo@gmail.com -p ~/repos/imap-contacts-cj/pass -s https://imap.gmail.com:993 -q"
 ;; End:
